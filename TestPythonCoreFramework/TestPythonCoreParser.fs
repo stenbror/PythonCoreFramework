bc237c2f94ddc63fbf2ae643ceec320e72fdfeaa
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-05-01 01:28:33 +0200

    Added UnitTest for for statement with typecomment and else statement.

--- TestPythonCoreFramework/TestPythonCoreParser.fs
+++ TestPythonCoreFramework/TestPythonCoreParser.fs
@@ -1988,3 +1988,23 @@ module TestsPythonCoreParser =
                                                             ASTNode.SimpleStmtList(34, 42, [| ASTNode.Pass(34, 39, Token.Pass(34, 38, [| |])) |], [| |], Token.Newline(39, 41, [| |]))
                                             )
                                             ), parser.ParseStmt())
+
+    [<Fact>]
+    let ``for statement with else part and typecomment test`` () =
+           let lex = new MockTokenizer( [ ( Token.For(0, 3, [| |]), 0 ); ( Token.Name(4, 8, "Test", [| |]), 4 ); ( Token.In(9, 11, [| |]), 9 ); ( Token.Name(12, 16, "Tell", [| |]), 12 ); ( Token.Colon(17, 18, [| |]), 17 ); ( Token.TypeComment(19, 26, "comment"), 19 ); ( Token.Pass(27, 31, [| |]), 27 ); ( Token.Newline(32, 34, [| |]), 32 ); 
+                                            ( Token.Else(35, 39, [| |]), 35 ); ( Token.Colon(40, 41, [| |]), 40 ); ( Token.Pass(42, 46, [| |]), 42 ); ( Token.Newline(47, 49, [| |]), 47 ); ( Token.EOF([| |]), 50 ); ] )
+           lex.Next()
+           let parser = new Parser(lex)
+           Assert.Equal( ASTNode.For(0, 50, Token.For(0, 3, [| |]),
+                                            ASTNode.ExprList(4, 9, [| ASTNode.Name(4, 9, Token.Name(4, 8, "Test", [| |])) |], [| |]),
+                                            Token.In(9, 11, [| |]),
+                                            ASTNode.TestList(12, 17, [| ASTNode.Name(12, 17, Token.Name(12, 16, "Tell", [| |])) |], [| |]),
+                                            Token.Colon(17, 18, [| |]),
+                                            Token.TypeComment(19, 26, "comment"),
+                                            ASTNode.SimpleStmtList(27, 35, [| ASTNode.Pass(27, 32, Token.Pass(27, 31, [| |])) |], [| |], Token.Newline(32, 34, [| |])),
+                                            ASTNode.Else(35, 50, 
+                                                            Token.Else(35, 39, [| |]),
+                                                            Token.Colon(40, 41, [| |]),
+                                                            ASTNode.SimpleStmtList(42, 50, [| ASTNode.Pass(42, 47, Token.Pass(42, 46, [| |])) |], [| |], Token.Newline(47, 49, [| |]))
+                                            )
+                                            ), parser.ParseStmt())
