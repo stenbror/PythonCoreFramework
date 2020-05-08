84412c19e64a3764b963a6506b83459e3b5115bd
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-05-08 21:21:08 +0200

    Added UnitTest for SyntaxError in Star Expr.

--- TestPythonCoreFramework/TestPythonCoreParserSyntaxErrorHandling.fs
+++ TestPythonCoreFramework/TestPythonCoreParserSyntaxErrorHandling.fs
@@ -24,3 +24,17 @@ module TestPythonCoreParserSyntaxErrorHandling =
         |   _ ->
                 Assert.False(false)
 
+    [<Fact>]
+    let ``star expr UnitTest`` () =
+        try
+            let lex = new MockTokenizer( [ ( Token.Comma(0, 1, [| |]), 0 ); ( Token.EOF([| |]), 2 ); ] )
+            lex.Next()
+            let parser = new Parser(lex)
+            parser.ParseStarExpr() |> ignore
+        with
+        |   :? SyntaxError as ex ->
+                Assert.Equal( Token.Comma(0, 1, [| |]), ex.Data0)
+                Assert.Equal( "Expecting '*' in expression!", ex.Data1)
+        |   _ ->
+                Assert.False(false)
+
